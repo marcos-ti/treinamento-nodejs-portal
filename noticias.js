9dabd723eed13338a5832245d864436ff667b7b9
Marcos <saoroque@hotmail.com>
2021-08-05 22:31:35 -0300

    Criando requisicoes de urls

--- noticias.js
+++ noticias.js
@@ -2,7 +2,22 @@
 var http = require('http');
 // criando um servidor com o metodo createServer
 var server = http.createServer(function( req, resp){
-    resp.end("<html><body>Portal de notícias</body></html>");
+// Resgata as urls das categorias que serao criadas (após o parametro req)
+    var categoria = req.url;
+    
+    if (categoria == `/tecnologia`){
+        resp.end("<html><body>Notícias de Tecnologia</body></html>");
+
+    } else if (categoria == `/moda`){
+        resp.end("<html><body>Notícias de Moda</body></html>");
+    
+    } else if (categoria == `/beleza`){
+        resp.end("<html><body>Notícias de Beleza</body></html>");
+    } else {
+        resp.end("<html><body>Portal de notícias</body></html>");
+    }
+
+    
 });
 
 server.listen(3000);
