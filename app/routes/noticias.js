a85fa40130d18358cc0b3628e811c4c9dfe5680c
Marcos <saoroque@hotmail.com>
2021-08-10 22:34:53 -0300

    Conectando com DB

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,7 +1,24 @@
  // Modularizando a rota. Tem que chamar o app como paramentro na funcao
  module.exports = function(app) {
+
+     // Encapsulando a instrucao de DB
     app.get('/noticias', function(req, res){
-        res.render(`noticias/noticias`);
+
+        // Conectando com o DB via modulo Mysql
+        var mysql = require('mysql');
+        // createConnection com parametros em estrutura Json chave : valor
+        var connection = mysql.createConnection({
+            host : 'localhost',
+            user : 'root',
+            password : '1234',
+            database : 'portal_noticias'
+        });
+
+        connection.query('select * from noticias', function(error, result){
+            //enviar os resultados da consulta
+            res.send(result);
+        });    
+        //res.render(`noticias/noticias`);
     });
- }
+ };
     
