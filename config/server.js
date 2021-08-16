f5adc46f6156fd59b46f3b160bc3fb05455aa10b
Marcos <saoroque@hotmail.com>
2021-08-15 21:51:54 -0300

    Middleware Body Parser Correcao

--- config/server.js
+++ config/server.js
@@ -28,3 +28,24 @@ consign()
 
 // Modulo retornando a variavel app
 module.exports = app;
+
+// BodyParse esta no Express nas versoes mais novas
+
+// var express = require('express');
+
+// var consign = require('consign');
+
+// var app = express();
+
+// app.set('view engine', 'ejs');
+// app.set('views', './app/views');
+
+// app.use(express.urlencoded({extended: true}));
+
+// consign()
+//     .include('app/routes')
+//     .then('config/dbConnection.js')
+//     .then('app/models')
+//     .into(app);
+// module.exports = app;
+
